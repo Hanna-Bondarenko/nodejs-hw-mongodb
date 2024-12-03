f996d75eaea84dd07e8b0d5d8cebfc554ca0846f
Hanna Bondarenko <annaleshhuk@gmail.com>
2024-12-03 11:13:34 +0200

    done hw

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,5 @@
-export const PATH_DB =
+import * as path from 'node:path';
+
+// export const PATH_DB_SONGS = path.join(process.cwd(), "src", "db", "db-songs.json");
+
+export const PATH_DB = path.resolve('src', 'db', 'db.json');
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,5 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = (contacts) =>
+  fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
