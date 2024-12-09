3a6cfebd14e21413c2c2ca8d39fa29500d545a06
Hanna Bondarenko <annaleshhuk@gmail.com>
2024-12-09 09:23:52 +0200

    init project

--- src/constants/contacts.js
+++
@@ -1,5 +0,0 @@
-import * as path from 'node:path';
-
-// export const PATH_DB_SONGS = path.join(process.cwd(), "src", "db", "db-songs.json");
-
-export const PATH_DB = path.resolve('src', 'db', 'db.json');
