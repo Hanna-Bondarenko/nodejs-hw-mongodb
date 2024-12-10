9e8b12ca3ba5af10d3e8dc15bba9865803b41656
Hanna Bondarenko <annaleshhuk@gmail.com>
2024-12-10 09:23:41 +0200

    Initialize empty main branch

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { startServer } from './server.js';
-import { initMongoDB } from './db/initMongoDB.js';
-
-const boostrap = async () => {
-  await initMongoDB(); //підключаємося до бази
-  startServer(); // запускаємо вебсервер
-};
-
-boostrap();
--- src/services/contacts-service.js
+++
@@ -1,5 +0,0 @@
-import { ContactsCollection } from '../db/models/Contacts.js';
-
-export const getContacts = () => ContactsCollection.find();
-
-export const getContactById = (id) => ContactsCollection.findById(id);
