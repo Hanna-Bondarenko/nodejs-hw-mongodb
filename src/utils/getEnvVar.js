caaf72be6098a66c8db31d61b6114c3da07c8678
Hanna Bondarenko <annaleshhuk@gmail.com>
2025-01-14 09:27:52 +0200

    HW-6-done

--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
 
-export const getEnvVar = (name, defaultValue) => {
+export function getEnvVar(name, defaultValue) {
   const value = process.env[name];
 
   if (value) return value;
+
   if (defaultValue) return defaultValue;
 
-  throw new Error(`Missing ${name} environment variable`);
-};
+  throw new Error(`Missing: process.env['${name}'].`);
+}
